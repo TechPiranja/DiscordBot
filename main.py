f021261683a6060e253ade4a6af5a4e2992362ad
Anja-Stricker <techpiranja@gmail.com>
2021-03-06 19:23:20 +0000

    can now push quotes with http request

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import discord
 import os
+import requests
+import json
 
 client = discord.Client()
+
+def get_quote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + ' -' + json_data[0]['a']
+  return quote
 
 @client.event
 async def on_ready():
@@ -12,7 +20,7 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('!hello'):
-    await message.channel.send('hello')
+  if message.content.startswith('!inspire'):
+    await message.channel.send(get_quote())
 
 client.run(os.getenv('TOKEN'))
